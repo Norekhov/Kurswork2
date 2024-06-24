d10ed814e6a868a1af2e16d5b958d61c8efc2b9d
nikit <nikit.orekhoff@yandex.ru>
2024-06-24 20:10:41 +0300

    Курсовая работа 2.0

---
+++ src/main/java/com/exam_questions/exam/questions/ExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.exam_questions.exam.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionsApplication.class, args);
+	}
+
+}
